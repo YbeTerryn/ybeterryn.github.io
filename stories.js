0427c462447b304476f2b5ca1819781684305f29
YbeTerryn <raamvertelling@outlook.com>
2025-12-13 18:58:19 +0100

    Update stories.js

--- stories.js
+++ stories.js
@@ -3,8 +3,4 @@ const stories = [
     title: "Window I – The First Night",
     pdf: "pdfs/window-01.pdf"
   },
-  {
-    title: "Window II – The Second Candle",
-    pdf: "pdfs/window-02.pdf"
-  }
 ];
