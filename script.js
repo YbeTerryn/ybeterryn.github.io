9b0e4f3b4c27315c910989f3527257200f678053
YbeTerryn <raamvertelling@outlook.com>
2025-12-13 18:57:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,11 +7,34 @@ const closeBtn = document.getElementById("close");
 const hour = new Date().getHours();
 const isNight = hour >= 19 || hour <= 6;
 
+// Convert number to Roman numeral
+function toRoman(num) {
+  const romans = [
+    ["M",1000],["CM",900],["D",500],["CD",400],
+    ["C",100],["XC",90],["L",50],["XL",40],
+    ["X",10],["IX",9],["V",5],["IV",4],["I",1]
+  ];
+  let result = "";
+  romans.forEach(([r, v]) => {
+    while (num >= v) {
+      result += r;
+      num -= v;
+    }
+  });
+  return result;
+}
+
 stories.forEach((story, index) => {
   const w = document.createElement("div");
   w.className = "window";
 
   if (isNight) w.classList.add("lit");
+
+  // Window number
+  const num = document.createElement("div");
+  num.className = "window-number";
+  num.textContent = toRoman(index + 1);
+  w.appendChild(num);
 
   w.onclick = () => {
     w.classList.add("open");
