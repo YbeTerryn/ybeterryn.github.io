9f6061160bff063cd9d45cd733b096f02197a0f0
YbeTerryn <raamvertelling@outlook.com>
2025-12-14 01:12:42 +0100

    Update stories.js

--- stories.js
+++ stories.js
@@ -1,6 +1,6 @@
 const stories = [
   {
     title: "Offer 1",
-    pdf: "file:///C:/Users/Ybe%20Terryn/OneDrive%20-%20UGent/Documenten/Schrijver/Sesjats%20Offerande.pdf"
+    pdf: "Sesjats Offerande.pdf"
   },
 ];
