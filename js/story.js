76ef560ecf6fe956e9d70b015541cca6acc2f9f0
YbeTerryn <raamvertelling@outlook.com>
2025-12-16 03:16:54 +0800

    Update story.js

--- js/story.js
+++ js/story.js
@@ -3,5 +3,12 @@ const id = params.get("id");
 
 const story = stories.find(s => s.id === id);
 
-document.getElementById("title").textContent = story.title;
-document.getElementById("reader").src = story.pdf;
+// laad tekst
+fetch(story.text)
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById("text-container").innerHTML = html;
+  });
+
+// PDF download link
+document.getElementById("pdf-link").href = story.pdf;
