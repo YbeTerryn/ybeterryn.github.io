e6c766aeefcc165d923fbc8840498fe1d07be338
YbeTerryn <raamvertelling@outlook.com>
2025-12-16 03:57:55 +0800

    Update render-offers.js

--- js/render-offers.js
+++ js/render-offers.js
@@ -1,12 +1,12 @@
 const container = document.getElementById("offers");
 
 stories.forEach(story => {
-  const card = document.createElement("div");
+  const card = document.createElement("article");
   card.className = "card";
   card.innerHTML = `
     <h2>${story.title}</h2>
-    <p>${story.date}</p>
-    <a href="story.html?id=${story.id}">Breng dit offer</a>
+    <p class="meta">${story.date}</p>
+    <a href="story.html?id=${story.id}">Lees</a>
   `;
   container.appendChild(card);
 });
