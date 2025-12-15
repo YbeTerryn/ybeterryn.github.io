926397524c7ae021e1043252701cdd139e2aae5b
YbeTerryn <raamvertelling@outlook.com>
2025-12-16 03:56:27 +0800

    Update stories.js

--- data/stories.js
+++ data/stories.js
@@ -3,8 +3,8 @@ const stories = [
     id: "offer-01",
     title: "Sesjats Offerande",
     date: "januari 2025",
-    text: "texts/offer-01.html",
-    pdf: "pdfs/offer-01.pdf"
+    text: "texts/offer-01.html"
   }
 ];
+
 
