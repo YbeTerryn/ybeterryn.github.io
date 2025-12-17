bf63c3bc9a9a1bf892610a96ee19ca572aba90dd
YbeTerryn <raamvertelling@outlook.com>
2025-12-17 15:50:03 +0100

    Update stories.js

--- data/stories.js
+++ data/stories.js
@@ -2,7 +2,7 @@ const stories = [
   {
     id: "Prélude",
     title: "Sesjats Offerande",
-    image: "Draft/Sesjat.jpg"
+    image: "Draft/Sesjat.jpg",
     date: "Prélude",
     text: "texts/offer-01.html"
   }
