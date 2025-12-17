d477a1c759786c3f1506bb5eac1e9440387dbda0
YbeTerryn <raamvertelling@outlook.com>
2025-12-17 18:36:14 +0100

    Update stories.js

--- data/stories.js
+++ data/stories.js
@@ -20,60 +20,11 @@ const archiveStories = [
 
 const reviewStories = [
   {
-    title: "O Agente Secreto",
-    year: "2025",
-    director: "Kleber Mendonça Filho",
-    image: "Draft/secretagent.jpg",
-    link: "https://letterboxd.com/pieterpaultybbe/film/the-secret-agent-2025/"
-  },
-  {
-    title: "Paris Is Burning",
-    year: "1990",
-    director: "Jennie Livingston",
-    image: "Draft/Paris.jpg",
-    link: "https://letterboxd.com/pieterpaultybbe/film/paris-is-burning/"
-  },
-  {
-    title: "Den Sidste Viking",
-    year: "2025",
-    director: "Anders Thomas Jensen",
-    image: "Draft/viking.jpg",
-    link: "https://letterboxd.com/pieterpaultybbe/film/the-last-viking-2025/"
-  },
-  {
-    title: "Babylon",
-    year: "2022",
-    director: "Damien Chazelle",
-    image: "Draft/babylon.jpg",
-    link: "https://letterboxd.com/pieterpaultybbe/film/babylon-2022/"
-  },
-  {
-    title: "Zabriskie Point",
-    year: "1970",
-    director: "Michelangelo Antonioni",
-    image: "Draft/point.jpg",
-    link: "https://letterboxd.com/pieterpaultybbe/film/zabriskie-point/"
-  },
-  {
-    title: "Szamanka",
-    year: "1996",
-    director: "Andrzej Żuławski",
-    image: "Draft/szamanka.jpg",
-    link: "https://letterboxd.com/pieterpaultybbe/film/szamanka/"
-  },
-  {
-    title: "Dr. Caligari",
-    year: "1986",
-    director: "Stephen Sayadian",
-    image: "Draft/caligari.jpg",
-    link: "https://letterboxd.com/pieterpaultybbe/film/dr-caligari/"
-  },
-  {
-    title: "À bout de souffle",
-    year: "1960",
-    director: "Jean-Luc Godard",
-    image: "Draft/bout.jpg",
-    link: "https://letterboxd.com/pieterpaultybbe/film/breathless/"
+    title: "La Vie sexuelle des Belges 1950–1978",
+    year: "1994",
+    director: "Jan Bucquoy",
+    image: "Draft/vie.jpg",
+    link: "https://letterboxd.com/pieterpaultybbe/film/the-sexual-life-of-the-belgians/"
   },
   {
     title: "Napoleon",
@@ -83,10 +34,59 @@ const reviewStories = [
     link: "https://letterboxd.com/pieterpaultybbe/film/napoleon/"
   },
   {
-    title: "La Vie sexuelle des Belges 1950–1978",
-    year: "1994",
-    director: "Jan Bucquoy",
-    image: "Draft/vie.jpg",
-    link: "https://letterboxd.com/pieterpaultybbe/film/the-sexual-life-of-the-belgians/"
+    title: "À bout de souffle",
+    year: "1960",
+    director: "Jean-Luc Godard",
+    image: "Draft/bout.jpg",
+    link: "https://letterboxd.com/pieterpaultybbe/film/breathless/"
+  },
+  {
+    title: "Dr. Caligari",
+    year: "1986",
+    director: "Stephen Sayadian",
+    image: "Draft/caligari.jpg",
+    link: "https://letterboxd.com/pieterpaultybbe/film/dr-caligari/"
+  },
+  {
+    title: "Szamanka",
+    year: "1996",
+    director: "Andrzej Żuławski",
+    image: "Draft/szamanka.jpg",
+    link: "https://letterboxd.com/pieterpaultybbe/film/szamanka/"
+  },
+  {
+    title: "Zabriskie Point",
+    year: "1970",
+    director: "Michelangelo Antonioni",
+    image: "Draft/point.jpg",
+    link: "https://letterboxd.com/pieterpaultybbe/film/zabriskie-point/"
+  },
+  {
+    title: "Babylon",
+    year: "2022",
+    director: "Damien Chazelle",
+    image: "Draft/babylon.jpg",
+    link: "https://letterboxd.com/pieterpaultybbe/film/babylon-2022/"
+  },
+  {
+    title: "Den Sidste Viking",
+    year: "2025",
+    director: "Anders Thomas Jensen",
+    image: "Draft/viking.jpg",
+    link: "https://letterboxd.com/pieterpaultybbe/film/the-last-viking-2025/"
+  },
+  {
+    title: "Paris Is Burning",
+    year: "1990",
+    director: "Jennie Livingston",
+    image: "Draft/Paris.jpg",
+    link: "https://letterboxd.com/pieterpaultybbe/film/paris-is-burning/"
+  },
+  {
+    title: "O Agente Secreto",
+    year: "2025",
+    director: "Kleber Mendonça Filho",
+    image: "Draft/secretagent.jpg",
+    link: "https://letterboxd.com/pieterpaultybbe/film/the-secret-agent-2025/"
   }
 ];
