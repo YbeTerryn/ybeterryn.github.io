f4ff80820ed43fea279e8456f08e7998b176e73a
YbeTerryn <raamvertelling@outlook.com>
2026-06-13 14:25:46 +0200

    Update stories.js

--- data/stories.js
+++ data/stories.js
@@ -138,51 +138,7 @@ const archiveStories = [
   }
 ];
 const reviewStories = [
-  {
-    title: "La Vie sexuelle des Belges 1950–1978",
-    year: "1994",
-    director: "Jan Bucquoy",
-    image: "Draft/vie.jpg",
-    rating: 4,
-    link: "https://letterboxd.com/pieterpaultybbe/film/the-sexual-life-of-the-belgians/",
-    platform: "letterboxd"
-  },
-  {
-    title: "Napoleon",
-    year: "1927",
-    director: "Abel Gance",
-    image: "Draft/napoleon.jpg",
-    rating: 4,
-    link: "https://letterboxd.com/pieterpaultybbe/film/napoleon/",
-    platform: "letterboxd"
-  },
-  {
-    title: "À bout de souffle",
-    year: "1960",
-    director: "Jean-Luc Godard",
-    image: "Draft/bout.jpg",
-    rating: 4.5,
-    link: "https://letterboxd.com/pieterpaultybbe/film/breathless/",
-    platform: "letterboxd"
-  },
-  {
-    title: "Dr. Caligari",
-    year: "1986",
-    director: "Stephen Sayadian",
-    image: "Draft/caligari.jpg",
-    rating: 3.5,
-    link: "https://letterboxd.com/pieterpaultybbe/film/dr-caligari/",
-    platform: "letterboxd"
-  },
-  {
-    title: "Szamanka",
-    year: "1996",
-    director: "Andrzej Żuławski",
-    image: "Draft/szamanka.jpg",
-    rating: 3,
-    link: "https://letterboxd.com/pieterpaultybbe/film/szamanka/",
-    platform: "letterboxd"
-  },
+
     {
     title: "Max, Mischa & het Tet-offensief",
     year: "2015",
@@ -191,24 +147,6 @@ const reviewStories = [
       rating: 4.5,
     link: "https://app.thestorygraph.com/reviews/a6b913f0-bcd7-4ed1-a432-f402c2061222",
     platform: "storygraph"
-  },
-  {
-    title: "Zabriskie Point",
-    year: "1970",
-    director: "Michelangelo Antonioni",
-    image: "Draft/point.jpg",
-    rating: 4.5,
-    link: "https://letterboxd.com/pieterpaultybbe/film/zabriskie-point/",
-    platform: "letterboxd"
-  },
-  {
-    title: "Babylon",
-    year: "2022",
-    director: "Damien Chazelle",
-    image: "Draft/babylon.jpg",
-    rating: 4,
-    link: "https://letterboxd.com/pieterpaultybbe/film/babylon-2022/",
-    platform: "letterboxd"
   },
  {
     title: "Sirenen",
@@ -219,24 +157,7 @@ const reviewStories = [
     link: "https://app.thestorygraph.com/reviews/59fb3d85-8a88-4404-ab12-c0d551ea8618",
     platform: "storygraph"
   },
-  {
-    title: "Den Sidste Viking",
-    year: "2025",
-    director: "Anders Thomas Jensen",
-    image: "Draft/viking.jpg",
-    rating: 4,
-    link: "https://letterboxd.com/pieterpaultybbe/film/the-last-viking-2025/",
-    platform: "letterboxd"
-  },
-    {
-    title: "The Texas Chain Saw Massacre",
-    year: "1974",
-    director: "Tobe Hooper",
-    image: "Draft/texas.jpg",
-      rating: 2,
-    link: "https://letterboxd.com/pieterpaultybbe/film/the-texas-chain-saw-massacre/",
-    platform: "letterboxd"
-  },
+ 
   {
    title: "Grand Hotel Europa",
     year: "2018",
@@ -247,15 +168,6 @@ const reviewStories = [
     platform: "storygraph"
   },
 
-  {
-    title: "Paris Is Burning",
-    year: "1990",
-    director: "Jennie Livingston",
-    image: "Draft/Paris.jpg",
-    rating: 4.5,
-    link: "https://letterboxd.com/pieterpaultybbe/film/paris-is-burning/",
-    platform: "letterboxd"
-  },
     {
     title: "Leven om het te vertellen",
     year: "2002",
@@ -264,19 +176,9 @@ const reviewStories = [
       rating: 3.25,
     link: "https://app.thestorygraph.com/reviews/8109815d-7157-4bd9-bfff-7da7d2b465bb",
     platform: "storygraph"
-  },
-  
-  {
-    title: "O Agente Secreto",
-    year: "2025",
-    director: "Kleber Mendonça Filho",
-    image: "Draft/secretagent.jpg",
-    rating: 4.5,
-    link: "https://letterboxd.com/pieterpaultybbe/film/the-secret-agent-2025/",
-    platform: "letterboxd"
-  },
+  },  
 {
-    title: "Beladonna of Sadness",
+    title: "Belladonna of Sadness",
     year: "1973",
     director: "Eiichi Yamamoto",
     image: "Draft/belladonna.jpg",
