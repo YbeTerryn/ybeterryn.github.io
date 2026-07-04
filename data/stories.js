abf73d947afb541461cd2cefa5abb1b1448f00e6
YbeTerryn <raamvertelling@outlook.com>
2026-07-04 18:29:43 +0200

    Add new story entry for 'Ctrl+alt+del'

--- data/stories.js
+++ data/stories.js
@@ -46,6 +46,12 @@ const stories = [
     date: "Offer 6",
     text: "texts/kroniekvaneenfabriek.html" // Dit klopt, want op GitHub is dit ook kleine letters
   },
+            {
+    id: "Offer 7",
+    title: "Ctrl+alt+del",
+    date: "Offer 7",
+    text: "texts/ctrl-alt-del.html" // Dit klopt, want op GitHub is dit ook kleine letters
+  },
 ];
 
 const archiveStories = [
